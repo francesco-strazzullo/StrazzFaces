362da5ea658e0cfe7405c9016d4d7d5907623b8a
Francesco Strazzullo <me@francescostrazzullo.info>
2014-03-13 23:00:48 +0100

    Implementato Clock con Raphael

--- StrazzFaces/src/it/strazz/faces/clock/Clock.java
+++ StrazzFaces/src/it/strazz/faces/clock/Clock.java
@@ -1,14 +1,27 @@
 package it.strazz.faces.clock;
 
+import java.util.Date;
+
+import javax.faces.application.ResourceDependencies;
 import javax.faces.application.ResourceDependency;
 import javax.faces.component.FacesComponent;
 import javax.faces.component.UIComponentBase;
+import javax.faces.component.UINamingContainer;
+import javax.faces.context.FacesContext;
 
-import com.sun.faces.application.ApplicationImpl;
+import org.primefaces.component.api.Widget;
 
 @FacesComponent(value=Clock.COMPONENT_TYPE)
-@ResourceDependency(library="strazzfaces/coolclock",name="coolclock.js")
-public class Clock extends UIComponentBase {
+@ResourceDependencies({
+	@ResourceDependency(library="coolclock",name="coolclock.js"),
+	@ResourceDependency(library="moment",name="moment.min.js"),
+	@ResourceDependency(library="underscore",name="underscore-min.js"),
+	@ResourceDependency(library="primefaces", name="jquery/jquery.js"),	
+	@ResourceDependency(library="raphael", name="raphael-min.js"),
+	@ResourceDependency(library="strazzfaces",name="clock.js")
+})
+
+public class Clock extends UIComponentBase implements Widget {
 
 	public static final String COMPONENT_TYPE = "it.strazz.faces.Clock";
 	public static final String COMPONENT_FAMILY = "it.strazz.faces.components";
@@ -25,8 +38,36 @@ public class Clock extends UIComponentBase {
 		this.getStateHelper().put(PropertyKeys.digital, digital);
 	}
 	
+	public String getPattern() {
+		return (String) getStateHelper().eval(PropertyKeys.pattern, "HH:mm:ss");
+	}
+	
+	public void setStartTime(Date _pattern) {
+		getStateHelper().put(PropertyKeys.startTime, _pattern);
+	}
+	
+	public Date getStartTime() {
+		return (Date) getStateHelper().eval(PropertyKeys.startTime);
+	}
+	
+	public void setPattern(String _pattern) {
+		getStateHelper().put(PropertyKeys.pattern, _pattern);
+	}
+	
 	protected static enum PropertyKeys{
-		digital;
+		startTime,
+		digital,
+		pattern;
+	}
+
+	public String resolveWidgetVar() {
+		FacesContext context = getFacesContext();
+		String userWidgetVar = (String) getAttributes().get("widgetVar");
+
+		if(userWidgetVar != null)
+			return userWidgetVar;
+		 else
+			return "widget_" + getClientId(context).replaceAll("-|" + UINamingContainer.getSeparatorChar(context), "_");
 	}
 
 }
--- StrazzFaces/src/it/strazz/faces/clock/ClockRenderer.java
+++ StrazzFaces/src/it/strazz/faces/clock/ClockRenderer.java
@@ -1,30 +1,82 @@
 package it.strazz.faces.clock;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
 import javax.faces.render.FacesRenderer;
-import javax.faces.render.Renderer;
+
+import org.primefaces.renderkit.CoreRenderer;
 
 @FacesRenderer(componentFamily=Clock.COMPONENT_FAMILY,rendererType=ClockRenderer.RENDERER_TYPE)
-public class ClockRenderer extends Renderer {
+public class ClockRenderer extends CoreRenderer {
 	
 	public static final String RENDERER_TYPE = "it.strazz.faces.ClockRenderer";
 
-	public void encodeBegin(FacesContext context, UIComponent component) throws IOException {
+	public void encodeEnd(FacesContext context, UIComponent component) throws IOException {
 		
 		Clock clock = (Clock) component;
 		
-		if(clock.isDigital()){
-			context.getResponseWriter().write("<div id='" + component.getClientId() + "'></div>");
-			context.getResponseWriter().write("<script>document.getElementById('" + component.getClientId() + "').innerHTML = (new Date()).toString();setInterval(function(){document.getElementById('" + component.getClientId() + "').innerHTML = (new Date()).toString();},1000);</script>");
-		}else{
-			context.getResponseWriter().write("<canvas id='" + component.getClientId() + "' class='CoolClock'></canvas>");
-			context.getResponseWriter().write("<script>CoolClock.findAndCreateClocks();</script>");
+		String widgetVar = clock.resolveWidgetVar();
+		
+		ResponseWriter writer = context.getResponseWriter();
+		
+		context.getResponseWriter().write("<div id=\"" + component.getClientId() + "\"></div>");
+		
+		startScript(writer, component.getClientId());
+		
+		writer.write("if(!" + widgetVar + "){");
+		writer.write("var " + widgetVar + " = new window.Clock(" + this.writeCfgObject(createCfgMap(clock)) + ");");
+		writer.write("}else{" + widgetVar + ".digital = " + clock.isDigital() + ";}");
+		writer.write(widgetVar+".draw();");
+		
+		endScript(writer);	
+		
+	}
+	
+	private Map<String,Object> createCfgMap(Clock clock){
+		Map<String,Object> map = new HashMap<String,Object>();
+		
+		map.put("id", clock.getClientId());
+		map.put("pattern", clock.getPattern());
+		map.put("digital", clock.isDigital());
+		map.put("time", clock.getStartTime() != null ? clock.getStartTime().getTime() : null);
+		
+		return map;
+	}
+	
+	private String writeCfgObject(Map<String,Object> attrs){
+		StringBuilder sb = new StringBuilder("{");
+		
+		int i = 0;
+		for (Entry<String, Object> entry : attrs.entrySet()) {
+			
+			String key = entry.getKey();
+			Object value = entry.getValue();
+			
+			if(value != null){
+				if(value instanceof CharSequence){
+					value = "'" + value + "'";
+				}
+				
+				sb.append(key).append(":").append(value);
+				
+				if(i < attrs.size()-1){
+					sb.append(",");
+				}
+			}
+			
+			i++;
+		
 		}
 		
+		sb.append("}");
 		
+		return sb.toString();
 	}
 	
 }
