3d2082c6f02e7ffb6d993f1a9a3eb5babbc8c897
Roberto Izzi <f1l0@tiscali.it>
2014-05-11 21:22:27 +0200

    Start new ExtPanel Component

---
+++ StrazzFaces/src/META-INF/resources/strazzfaces/extpanel.js
@@ -0,0 +1,59 @@
+/**
+ * @author f1l0
+ */
+PrimeFaces.widget.ExtPanel = PrimeFaces.widget.BaseWidget.extend({
+
+    init: function(cfg) {
+
+        this._super(cfg);
+        this.name = cfg.widgetName;
+        this.position = cfg.position;
+        this.title = cfg.title;
+        this.isOpen = false;
+
+        var that = this;
+
+        jQuery(document).ready(function() {
+
+            if(that.position === "top" || that.position === "bottom") {
+                that.refreshTop();
+            }
+
+            that.bindEvent();
+        });
+
+    },
+
+    bindEvent: function() {
+        var _this = this;
+        jQuery(this.jqId).find("DIV.extpanel-header").click(function() { _this.toggle(); });
+    },
+
+    toggle: function() {
+        if(this.isOpen)
+            this.close();
+        else this.open();
+    },
+
+    refreshTop: function() {
+        jQuery(this.jqId).css("margin-left", "-"+ parseInt(jQuery(this.jqId).width()/2, 10) +"px");
+    },
+
+    open: function() {
+        if(!this.isOpen) {
+            jQuery(this.jqId).find("SPAN.ui-icon").removeClass("ui-icon-triangle-1-s").addClass("ui-icon-triangle-1-n");
+            jQuery(this.jqId).find("DIV.extpanel-content").css("display", "");
+            this.refreshTop();
+            this.isOpen = true;
+        }
+    },
+            
+    close: function() {
+        if(this.isOpen) {
+            jQuery(this.jqId).find("SPAN.ui-icon").removeClass("ui-icon-triangle-1-n").addClass("ui-icon-triangle-1-s");
+            jQuery(this.jqId).find("DIV.extpanel-content").css( "display","none");
+            this.refreshTop();
+            this.isOpen = false;
+        }
+    }
+});
