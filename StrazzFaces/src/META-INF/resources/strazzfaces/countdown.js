9a29dc461e28aae2a45d7c63f6c38adceaba63da
Roberto Izzi <f1l0@tiscali.it>
2014-05-14 21:09:28 +0200

    script cleaned

--- StrazzFaces/src/META-INF/resources/strazzfaces/countdown.js
+++ StrazzFaces/src/META-INF/resources/strazzfaces/countdown.js
@@ -4,39 +4,23 @@
 PrimeFaces.widget.CountDown = PrimeFaces.widget.BaseWidget.extend({
 
     init: function(cfg) {
-
         this._super(cfg);
         this.name = cfg.widgetName;
-//        this.dayText = cfg.dayText;
-//        this.daysText = cfg.daysText;
-//        this.hoursText = cfg.hoursText;
-//        this.minutesText = cfg.minutesText;
-//        this.secondsText = cfg.secondsText;
-//        this.oneDayClass = cfg.oneDayClass;
-//        this.textAfterCount = cfg.textAfterCount;
-//        this.displayDays = cfg.displayDays;
-//        this.displayZeroDays = cfg.displayZeroDays;
-//        this.addClass = cfg.addClass;
-//        this.callback = cfg.callback;
-        
-        //this.countdownJQ = jQuery(this.jqId);
-
         var that = this;
 
         jQuery(document).ready(function() {
-            
             jQuery(that.jqId).kkcountdown({
-                dayText         : cfg.dayText,
-                daysText        : cfg.daysText,
-                hoursText       : cfg.hoursText,
-                minutesText     : cfg.minutesText,
-                secondsText     : cfg.secondsText,
-                oneDayClass     : cfg.oneDayClass,
-                textAfterCount  : cfg.textAfterCount,
-                displayDays     : cfg.displayDays,
-                displayZeroDays : cfg.displayZeroDays,
-                addClass        : cfg.addClass,
-                callback        : cfg.callback
+                dayText         : that.cfg.dayText,
+                daysText        : that.cfg.daysText,
+                hoursText       : that.cfg.hoursText,
+                minutesText     : that.cfg.minutesText,
+                secondsText     : that.cfg.secondsText,
+                oneDayClass     : that.cfg.oneDayClass,
+                textAfterCount  : that.cfg.textAfterCount,
+                displayDays     : that.cfg.displayDays,
+                displayZeroDays : that.cfg.displayZeroDays,
+                addClass        : that.cfg.addClass,
+                callback        : that.cfg.callback
             });
         });
     }
