eeb10e6348afac2af749b160d0f88a047a85bc4a
Roberto Izzi <f1l0@tiscali.it>
2014-05-14 18:32:41 +0200

    new Util package/class

---
+++ StrazzFaces/src/it/strazz/faces/util/Util.java
@@ -0,0 +1,12 @@
+package it.strazz.faces.util;
+
+/**
+ * Util class
+ * @author f1l0
+ */
+public class Util {
+    
+    public static boolean isValid(String str) {
+        return str!=null && !str.isEmpty();
+    }
+}
