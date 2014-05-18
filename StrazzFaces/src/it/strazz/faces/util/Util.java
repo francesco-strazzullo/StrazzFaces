2a6558246b8e71d6e085be19d911ef730f0f5ea4
Roberto Izzi <f1l0@tiscali.it>
2014-05-18 23:35:26 +0200

    Add method to get the current PrimeFaces theme

--- StrazzFaces/src/it/strazz/faces/util/Util.java
+++ StrazzFaces/src/it/strazz/faces/util/Util.java
@@ -1,12 +1,23 @@
 package it.strazz.faces.util;
 
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.context.FacesContext;
+
 /**
  * Util class
  * @author f1l0
  */
-public class Util {
+@ApplicationScoped
+public final class Util {
     
     public static boolean isValid(String str) {
         return str!=null && !str.isEmpty();
+    }
+    
+    public static String getTheme() {
+        String theme = FacesContext.getCurrentInstance().getExternalContext().getInitParameter("primefaces.THEME");
+        if(isValid(theme))
+            return theme;
+        return "aristo";
     }
 }
